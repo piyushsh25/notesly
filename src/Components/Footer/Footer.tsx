517dba189478e5ededd40c6d409e5e91d54e76ff
piyush <piyushsharmashpr@gmail.com>
2022-10-23 07:33:04 +0530

    styles, changed the padding

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,9 +1,21 @@
-import "./Footer.css"
+import "./Footer.css";
 export const Footer = () => {
   return (
     <div className="footer-div">
-              <div className="footer-title"> Piyush Poudyal </div>
-        <div className="footer-icon"><i className="bi bi-box-arrow-right"></i></div>
+      <div>
+        <div className="footer-title"> Piyush Poudyal </div>
+        <div className="footer-icon">
+          <i className="bi bi-box-arrow-right"></i>
+        </div>
+      </div>
+      <div className="social-icons">
+        <div>
+          <i className="bi bi-github"></i>
+        </div>
+        <div>
+        <i className="bi bi-linkedin"></i>
+        </div>
+      </div>
     </div>
   );
 };
