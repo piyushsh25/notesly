0d48c5afc01c3cd572145343224d0a9b0af05fb0
piyush <piyushsharmashpr@gmail.com>
2022-12-02 11:34:59 +0530

    structured login types

--- src/Components/LoginPage/LoginBody.tsx
+++ src/Components/LoginPage/LoginBody.tsx
@@ -1,6 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { AppDispatch } from "../../Hooks/store";
+import {
+  loginActions,
+  loginHandler
+} from "../../Hooks/slices/loginSlice";
 import "../SignupPage/Signup.css";
+import { RootState } from "../../Hooks/store";
 export function LoginBody() {
+  const dispatch = useDispatch<AppDispatch>();
+  const { username, password } = useSelector(
+    (store: RootState) => store.loginReducer
+  );
   return (
     <div className="login-body">
       <div>or login using</div>
@@ -8,9 +19,12 @@ export function LoginBody() {
         <div className="username-container">
           <i className="bi bi-envelope email-icon"></i>
           <input
-            type="email"
-            placeholder="email-id"
+            type="text"
+            placeholder="username"
             className="enter-username"
+            onChange={(e) =>
+              dispatch(loginActions.setUsernameHandler(e.target.value))
+            }
           />
         </div>
         <div className="password-container">
@@ -19,11 +33,19 @@ export function LoginBody() {
             type="password"
             placeholder="password"
             className="enter-password"
+            onChange={(e) =>
+              dispatch(loginActions.setPasswordHandler(e.target.value))
+            }
           />
         </div>
-        <button className="submit-button">Submit</button>
+        <button
+          className="submit-button"
+          onClick={() => dispatch(loginHandler({ username, password }))}
+        >
+          Submit
+        </button>
         <Link to="/signup" className="login-signup-redirect">
-        New? Create a new account.
+          New? Create a new account.
         </Link>
       </div>
     </div>
---
+++ src/Hooks/store.tsx
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { loginReducer } from "./slices/loginSlice";
+export const store = configureStore({
+  reducer: { loginReducer }
+});
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch
