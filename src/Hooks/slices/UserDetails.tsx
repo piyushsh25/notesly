debc481df81d3e1d81090f5ec6a7fa84d6c6e22d
piyush <piyushsharmashpr@gmail.com>
2022-12-02 15:20:35 +0530

    [feat] authenticating user in the main page

---
+++ src/Hooks/slices/UserDetails.tsx
@@ -0,0 +1,35 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { UserState } from "./User.types";
+
+const initialState: UserState = {
+  name: "",
+  userstatus: "idle",
+  bio: [],
+};
+export const getDetails = createAsyncThunk("user/getDetails", async () => {
+  const token = localStorage.getItem("token");
+  const response = await axios.get("https://notesly-backend.onrender.com/me", {
+    headers: { authorization: token },
+  });
+  return response.data;
+});
+const UserSlice = createSlice({
+  name: "userdetails",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getDetails.pending, (state, action) => {
+      state.userstatus = "pending";
+    });
+    builder.addCase(getDetails.rejected, (state, action) => {
+      state.userstatus = "error";
+    });
+    builder.addCase(getDetails.fulfilled, (state, action) => {
+      state.name = action.payload.user[0].username;
+      state.userstatus = "succeeded";
+    });
+  },
+});
+export const userReducer = UserSlice.reducer;
+export const userActions = UserSlice.actions;
