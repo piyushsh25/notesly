2111c6f0d3dc747271451435b32cda4ed67b9edb
piyush <piyushsharmashpr@gmail.com>
2022-12-05 17:52:36 +0530

    [refactor] added font-family option

--- src/Components/NewNote/NewNoteForm.tsx
+++ src/Components/NewNote/NewNoteForm.tsx
@@ -1,6 +1,14 @@
 import "./NewNote.css";
 import Form from "react-bootstrap/Form";
 export const NewNoteForm = () => {
+  const availableFonts = [
+    "'Nunito Sans', sans-serif",
+    "'Fuzzy Bubbles', cursive;",
+    "'Lora', serif;",
+    "'PT Sans', sans-serif;",
+    "'Fasthand', cursive;",
+    "'Fuzzy Bubbles', cursive;",
+  ];
   return (
     <div className="new-note-body">
       <div className="new-note-container">
@@ -9,7 +17,9 @@ export const NewNoteForm = () => {
           <Form.Group className="mb-3" controlId="">
             <Form.Label> Title</Form.Label>
             <Form.Control type="text" placeholder="Enter title" />
-            <Form.Text className="text-muted">Atleast 10 char long.</Form.Text>
+            <Form.Text className="text-muted">
+              Title should be atleast 10 characters long.
+            </Form.Text>
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>Enter content</Form.Label>
@@ -18,28 +28,34 @@ export const NewNoteForm = () => {
           <Form.Group className="mb-3">
             <Form.Label>Select font-family</Form.Label>
             <Form.Select>
-              <option>Disabled select</option>
+              {availableFonts.map((font) => {
+                return <option>{font}</option>;
+              })}
             </Form.Select>
           </Form.Group>
           <Form.Group className="mb-3" controlId="">
             <Form.Label> Text box Color</Form.Label>
             <Form.Control type="color" placeholder="text-box-color" />
           </Form.Group>
-          <Form.Group>
+          <Form.Group className="pin-note-form-container">
             <Form.Label> Pin note ?</Form.Label>
             <div key={`default-radio`} className="mb-3">
-              <Form.Check
-                type="radio"
-                id={`default-radio`}
-                label="Yups"
-                name="pin-note"
-              />
-              <Form.Check
-                type="radio"
-                id={`default-radio`}
-                label="Nah"
-                name="pin-note"
-              />
+              <div>
+                <Form.Check
+                  type="radio"
+                  id={`default-radio`}
+                  label="Yups"
+                  name="pin-note"
+                />
+              </div>
+              <div>
+                <Form.Check
+                  type="radio"
+                  id={`default-radio`}
+                  label="Nah"
+                  name="pin-note"
+                />
+              </div>
             </div>
           </Form.Group>
           <Form.Group className="mb-3" controlId="">
