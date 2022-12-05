44175593f9bc1fa0e6bfef671e27242275714292
piyush <piyushsharmashpr@gmail.com>
2022-12-05 15:42:27 +0530

    [jwt bug] if jwt is altered, automatic logout

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,14 +5,23 @@ import { AppDispatch } from "../../Hooks/store";
 import "./Header.css";
 export const Header = () => {
   const navigate = useNavigate();
-const dispatch=useDispatch<AppDispatch>()
+  const dispatch = useDispatch<AppDispatch>();
   const logoutSelfHandler = () => {
-    navigate("/logout");
-    dispatch(toastAction.setMessage({header:"You have successfully logged out",description:"We will miss you, please come back soon.:)",color:"Info"}))
-    dispatch(toastAction.setToastHandler({message:true}))
-    setTimeout(()=>{
-      dispatch(toastAction.setToastHandler({message:false}))
-    },5000)
+    //to check if token exists and logout feat is not being used in login and signup pages
+    if (localStorage.getItem("token")) {
+      navigate("/logout");
+      dispatch(
+        toastAction.setMessage({
+          header: "You have successfully logged out",
+          description: "We will miss you, please come back soon.:)",
+          color: "Info",
+        })
+      );
+      dispatch(toastAction.setToastHandler({ message: true }));
+      setTimeout(() => {
+        dispatch(toastAction.setToastHandler({ message: false }));
+      }, 5000);
+    }
   };
   return (
     <div className="header-div">
@@ -20,7 +29,7 @@ const dispatch=useDispatch<AppDispatch>()
         <div className="header-title"> notesly </div>
       </div>
       <div>
-        <button className="header-icon" onClick={ logoutSelfHandler}>
+        <button className="header-icon" onClick={logoutSelfHandler}>
           <i className="bi bi-box-arrow-right"></i>
         </button>
       </div>
